024a49b1629f74b66cd47f73755fbe6728c51a59
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-09-06 22:31:11 +0200

    Duplicated key binding

--- key-bindings.el
+++ key-bindings.el
@@ -43,7 +43,6 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C->") 'mc/mark-all-like-this)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
 (global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
 ;; Rectangular region mode
