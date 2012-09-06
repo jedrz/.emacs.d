0a7c38f5ee4d57319a1f261c03428c7f1ada85af
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-09-06 18:55:03 +0200

    Organize key bindings

--- key-bindings.el
+++ key-bindings.el
@@ -17,30 +17,19 @@
 ;; Make enter to work as C-j
 (global-set-key (kbd "RET") 'newline-and-indent)
 
-;; Rebind C-a to back-to-indentation
-(global-set-key (kbd "C-a") 'back-to-indentation)
-
-;; Replace buffer-menu with ibuffer
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-;; Open recent files with ido
-(global-set-key (kbd "C-x f") 'recentf-ido-find-file)
-
 ;; Replace old M-x with smex
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
-(global-set-key (kbd "C-x ^") 'join-line)
-
-;; A complementary binding to the apropos-command(C-h a)
-(global-set-key (kbd "C-h A") 'apropos)
-
+;;; Motion
+;; Rebind C-a to back-to-indentation
+(global-set-key (kbd "C-a") 'back-to-indentation)
 ;; Jump to a definition in the current file
 (global-set-key (kbd "C-x C-i") 'ido-imenu)
 
-;; Edit file with sudo
-(global-set-key (kbd "M-s e") 'sudo-edit)
-
+;;; Editing
+;; Just join line
+(global-set-key (kbd "C-x ^") 'join-line)
 ;; Transpose stuff with M-t
 (global-unset-key (kbd "M-t")) ;; which used to be transpose-words
 (global-set-key (kbd "M-t l") 'transpose-lines)
@@ -48,9 +37,23 @@
 (global-set-key (kbd "M-t s") 'transpose-sexps)
 (global-set-key (kbd "M-t p") 'transpose-params)
 
+;;; File finding
+;; Find recent files with ido
+(global-set-key (kbd "C-x f") 'recentf-ido-find-file)
+;; Find file in other window
+(global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
+;; Edit file with sudo
+(global-set-key (kbd "M-s e") 'sudo-edit)
+;; Replace buffer-menu with ibuffer
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;;; Windows management
 ;; Use shift + arrow keys to switch between windows
 (windmove-default-keybindings)
 ;; Windows rotating
 (global-set-key (kbd "C-x w") 'rotate-windows)
+
+;; A complementary binding to the apropos-command(C-h a)
+(global-set-key (kbd "C-h A") 'apropos)
 
 (provide 'key-bindings)
