9c6c601ca6c57527bbafe5c1284a2902c013bb51
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-09-07 13:29:40 +0200

    Just move 2 key bindings

--- key-bindings.el
+++ key-bindings.el
@@ -40,11 +40,11 @@
 (global-set-key (kbd "M-t p") 'transpose-params)
 ;; Multiple cursors!
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
+(global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C->") 'mc/mark-all-like-this)
-(global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
-(global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
 ;; Rectangular region mode
 (global-set-key (kbd "C-S-SPC") 'set-rectangular-region-anchor)
 ;; Nice replacement of string-rectangle
