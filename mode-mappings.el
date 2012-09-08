6069a0c7e39e61712aff2a96b8fb49d0990d2635
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-09-08 11:03:21 +0200

    Mark and rename matching tags

--- mode-mappings.el
+++ mode-mappings.el
@@ -1,3 +1,8 @@
+;; HTML
+(add-hook 'sgml-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c C-r") 'rename-sgml-tag)))
+
 ;; Markdown
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\markdown$" . markdown-mode))
