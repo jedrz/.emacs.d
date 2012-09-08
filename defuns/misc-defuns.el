b2f5f4d8c6ba49bd25e9cb0be0db411172137b60
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-09-08 13:08:04 +0200

    That wasn't the best place

--- defuns/misc-defuns.el
+++ defuns/misc-defuns.el
@@ -6,9 +6,3 @@
   (if (or arg (not buffer-file-name))
       (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
-
-;; source: Prelude
-(defun add-watchwords ()
-  (font-lock-add-keywords
-   nil '(("\\<\\(FIX\\|TODO\\|FIXME\\|HACK\\|REFACTOR\\):"
-          1 font-lock-warning-face t))))
