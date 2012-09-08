f1cc7da7602df58ece268f6f9464c462be6ddfc1
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-09-08 11:08:17 +0200

    More consistent

--- mode-mappings.el
+++ mode-mappings.el
@@ -7,6 +7,7 @@
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\markdown$" . markdown-mode))
 (add-hook 'markdown-mode-hook
-          '(lambda () (define-key markdown-mode-map (kbd "<tab>") 'yas/expand)))
+          '(lambda ()
+             (define-key markdown-mode-map (kbd "<tab>") 'yas/expand)))
 
 (provide 'mode-mappings)
