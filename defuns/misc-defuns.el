91776cc3345975d5a116e39478603241c61ca2a6
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-09-08 21:50:03 +0200

    Add function to fetch url into new buffer

--- defuns/misc-defuns.el
+++ defuns/misc-defuns.el
@@ -18,3 +18,11 @@
                            (buffer-substring (region-beginning) (region-end))
                          (read-string "Google: "))))))
 ;; }}}
+
+(defun view-url ()
+  "Open a new buffer containing the contents of URL."
+  (interactive)
+  (let* ((default (thing-at-point-url-at-point))
+         (url (read-from-minibuffer "URL: " default)))
+    (switch-to-buffer (url-retrieve-synchronously url))
+    (rename-buffer url t)))
