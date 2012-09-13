492274b4cac8c25928f8753048cb76998795c1d7
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-09-13 18:14:51 +0200

    Shorten markdown mode mappings

--- setup-markdown-mode.el
+++ setup-markdown-mode.el
@@ -1,6 +1,4 @@
-;; Markdown
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\markdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(md\\|markdown\\)$" . markdown-mode))
 (add-hook 'markdown-mode-hook
           '(lambda ()
              (define-key markdown-mode-map (kbd "<tab>") 'yas/expand)))
