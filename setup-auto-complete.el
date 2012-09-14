0787f7175ad32b074184e1fa3826d4df381d8b74
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-09-14 22:16:18 +0200

    Fix interfering AC and completion-at-point

--- setup-auto-complete.el
+++ setup-auto-complete.el
@@ -7,8 +7,8 @@
 (global-auto-complete-mode 1)
 (ac-config-default)
 
-(setq ac-trigger-key "TAB"
-      ac-auto-start nil ; start only by hitting TAB
+(ac-set-trigger-key "TAB")
+(setq ac-auto-start nil ; start only by hitting TAB
       ac-dwim nil) ; To get pop-ups with docs even if a word is uniquely completed
 
 (define-key ac-completing-map (kbd "C-n") 'ac-next)
@@ -17,6 +17,11 @@
 ;; Use Emacs' built-in TAB completion hooks to trigger AC
 (setq tab-always-indent 'complete) ; use 'complete when auto-complete is disabled
 (add-to-list 'completion-styles 'initials t)
+
+;; hook AC into completion-at-point
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
 ;; Add auto-complete to some modes
 (add-to-list 'ac-modes 'markdown-mode)
