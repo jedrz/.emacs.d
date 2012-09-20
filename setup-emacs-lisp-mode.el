632676900c27de069c695c731d5582df452e432e
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-09-20 19:25:15 +0200

    Enable paredit in elisp mode

--- setup-emacs-lisp-mode.el
+++ setup-emacs-lisp-mode.el
@@ -1,9 +1,8 @@
 ;;; Basic configuration for emacs lisp mode
 
 (defun emacs-lisp-mode-defaults ()
-  ;; Arguments' names in minibuffer
   (turn-on-eldoc-mode)
-  ;; rainbow parentheses
+  (paredit-mode 1)
   (rainbow-delimiters-mode 1))
 
 (add-hook 'emacs-lisp-mode-hook 'emacs-lisp-mode-defaults)
