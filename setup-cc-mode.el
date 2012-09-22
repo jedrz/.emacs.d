3a0cad3c08807260381e9d8f893ada60e1fc5713
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-09-22 20:37:26 +0200

    Improve cc-mode configuration

--- setup-cc-mode.el
+++ setup-cc-mode.el
@@ -1,7 +1,13 @@
 ;;; Basic configuration for cc-mode and modes derived from it
 
 (eval-after-load 'cc-mode
-  '(setq c-default-style "linux"
-         c-basic-offset 4))
+  '(setq-default c-default-style "linux"
+                 c-basic-offset 4))
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            ;; Enable deleting all whitespace until next non-whitespace with DEL
+            ;; and auto inserting newlines after special characters
+            (c-toggle-auto-hungry-state 1)))
 
 (provide 'setup-cc-mode)
