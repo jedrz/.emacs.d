6e0217eb2d07bf0afa52a816612997c7c1a83fab
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-10-20 12:36:33 +0200

    Add basic c completion with semantic mode

--- setup-cc-mode.el
+++ setup-cc-mode.el
@@ -3,9 +3,15 @@
 (setq-default c-default-style "linux"
               c-basic-offset 4)
 
+(eval-after-load "semantic"
+  '(require 'semantic/bovine/c))
+
 (add-hook 'c-mode-common-hook
           (lambda ()
             ;; Enable deleting all whitespace until next non-whitespace
-            (c-toggle-hungry-state 1)))
+            (c-toggle-hungry-state 1)
+            ;; Set up completion
+            (semantic-mode 1)
+            (add-to-list 'ac-sources 'ac-source-semantic)))
 
 (provide 'setup-cc-mode)
