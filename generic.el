7bc2097dc261eba7ca46a90c05dfc92f4602249d
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-10-30 15:09:16 +0100

    One more function call to set up utf-8 coding

--- generic.el
+++ generic.el
@@ -47,6 +47,7 @@
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
+(set-language-environment 'utf-8)
 (prefer-coding-system 'utf-8)
 
 ;; Spaces for indentation
