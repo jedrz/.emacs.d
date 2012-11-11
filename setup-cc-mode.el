3f436cf089217a95a9ff9c012fcafec12ab839b6
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-11-11 19:05:21 +0100

    Disable semantic in cc modes

--- setup-cc-mode.el
+++ setup-cc-mode.el
@@ -5,15 +5,16 @@
                                 (other . "linux"))
               c-basic-offset 4)
 
-(eval-after-load "semantic"
-  '(require 'semantic/bovine/c))
+;; (eval-after-load "semantic"
+;;   '(require 'semantic/bovine/c))
 
 (add-hook 'c-mode-common-hook
           (lambda ()
             ;; Enable deleting all whitespace until next non-whitespace
             (c-toggle-hungry-state 1)
             ;; Set up completion
-            (semantic-mode 1)
-            (add-to-list 'ac-sources 'ac-source-semantic)))
+            ;; (semantic-mode 1)
+            ;; (add-to-list 'ac-sources 'ac-source-semantic)
+            ))
 
 (provide 'setup-cc-mode)
