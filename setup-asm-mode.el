16b59218661d20e309f9764c177b5d27c394883a
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2012-12-27 11:48:54 +0100

    Improve some asm utils

--- setup-asm-mode.el
+++ setup-asm-mode.el
@@ -10,23 +10,38 @@
   ;; Fix one level comments.
   (set (make-local-variable 'comment-start) (string asm-comment-char)))
 
-(defun asm-align (ins-column)
-  "Align asm instructions and comments.
+(defun asm-align (&optional column)
+  "Align asm instructions.
 
-Move text after every instruction to INS-COLUMN column
-and align inline comments to `comment-column'."
-  (interactive "p")
+Move text after every instruction to COLUMN or 12 if nil
+and fix inline comments by indenting to `comment-column'."
+  (interactive "P")
+  (setq column (if column
+                   (prefix-numeric-value column)
+                 12))
   (save-excursion
     ;; Align instructions.
     (goto-char (point-min))
-    (while (search-forward-regexp "^[ \t]+[^#:\.a-z]+[a-z]+" nil t)
-      (indent-to-column ins-column)
+    (while (search-forward-regexp "^[ \t]+[[:alnum:]]+" nil t)
+      (delete-horizontal-space)
+      (indent-to-column column)
       ;; Delete white space after instructions without arguments.
       (when (eolp)
-       (delete-horizontal-space)))
-    ;; Align inline comments.
+        (delete-horizontal-space))
+      ;; Fix inline comments.
+      (when (search-forward (format "%c" asm-comment-char)
+                            (line-end-position)
+                            t)
+        (indent-for-comment)))))
+
+(defun asm-convert-comment-chars-to-single ()
+  "Convert multiple comment chars to only single one."
+  (interactive)
+  (save-excursion
     (goto-char (point-min))
-    (while (search-forward-regexp (format " %c\\{1\\} " asm-comment-char) nil t)
-      (indent-for-comment))))
+    (while (search-forward-regexp (format "%c\\{2,3\\}" asm-comment-char)
+                                  nil
+                                  t)
+      (replace-match (char-to-string asm-comment-char)))))
 
 (provide 'setup-asm-mode)
