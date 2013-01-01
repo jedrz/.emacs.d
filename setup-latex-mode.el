4ce56e451819bd20b963d38999fff7ae68de9643
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2013-01-01 12:20:08 +0100

    Fix auctex initialization
    Fix some inline comments positions.

--- setup-latex-mode.el
+++ setup-latex-mode.el
@@ -1,48 +1,9 @@
 ;;; Latex configuration with Auctex
 
-;; {{{
-;; http://www.emacswiki.org/emacs/AUCTeX#toc20
-;; (require 'dbus)
-
-;; (defun un-urlify (fname-or-url)
-;;   "A trivial function that replaces a prefix of file:/// with just /."
-;;   (if (string= (substring fname-or-url 0 8) "file:///")
-;;       (substring fname-or-url 7)
-;;     fname-or-url))
-
-;; (defun th-evince-sync (file linecol &rest ignored)
-;;   (let* ((fname (un-urlify file))
-;;          (buf (find-buffer-visiting fname))
-;;          (line (car linecol))
-;;          (col (cadr linecol)))
-;;     (if (null buf)
-;;         (message "[Synctex]: %s is not opened..." fname)
-;;       (switch-to-buffer buf)
-;;       (goto-line (car linecol))
-;;       (unless (= col -1)
-;;         (move-to-column col)))))
-
-;; (defvar *dbus-evince-signal* nil)
-
-;; (defun enable-evince-sync ()
-;;   (require 'dbus)
-;;   (when (and
-;;          (eq window-system 'x)
-;;          (fboundp 'dbus-register-signal))
-;;     (unless *dbus-evince-signal*
-;;       (setf *dbus-evince-signal*
-;;             (dbus-register-signal
-;;              :session nil "/org/gnome/evince/Window/0"
-;;              "org.gnome.evince.Window" "SyncSource"
-;;              'th-evince-sync)))))
-
-;; (add-hook 'LaTeX-mode-hook 'enable-evince-sync)
-;; }}}
-
 ;; Configure AUCTex
-(eval-after-load "tex-site"
+(eval-after-load "tex"
   '(progn
-     (setq TeX-auto-save t ; Automatically save style information
+     (setq TeX-auto-save t              ; Automatically save style information
            ;; Parse document structure
            TeX-parse-self t
            ;; Use SyncTeX for source correlation
@@ -55,7 +16,7 @@
            TeX-clean-confirm nil
            ;; Please indent \item
            LaTeX-item-indent 0)
-     (setq-default TeX-master nil ; Ask for master document
+     (setq-default TeX-master nil       ; Ask for master document
                    ;; Generate output in PDF
                    TeX-PDF-mode t)))
 
@@ -69,8 +30,9 @@
            ;; Plug RefTeX into AUCTeX
            reftex-plug-into-AUCTeX t)))
 
-(add-hook 'LaTeX-mode-hook
+(add-hook 'latex-mode-hook
           (lambda ()
+            (TeX-latex-mode)          ; Enable auctex
             (flyspell-mode-on)
             (reftex-mode 1)
             (LaTeX-math-mode 1)))
