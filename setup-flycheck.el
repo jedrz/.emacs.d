7404f3b388a0d38525fe308baf9c100f73dffa2a
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2013-01-09 17:26:21 +0100

    Add docstring to checker declaration

--- setup-flycheck.el
+++ setup-flycheck.el
@@ -9,6 +9,7 @@
      ;; not the one being being checked actually)
      (defmacro flycheck-define-clike-checker (name command modes)
        `(flycheck-declare-checker ,(intern (format "flycheck-checker-%s" name))
+          ,(format "A %s checker using %s" name (car command))
           :command '(,@command source-inplace)
           :error-patterns
           '(("^\\(?1:.*\\):\\(?2:[0-9]+\\):\\(?3:[0-9]+\\): error: \\(?4:.*\\)$"
