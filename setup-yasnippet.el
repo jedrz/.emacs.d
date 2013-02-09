6898e945126504bc0ceb44ada302024925aac92c
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2013-02-09 09:56:55 +0100

    Do not use aliases to set up yasnippet

--- setup-yasnippet.el
+++ setup-yasnippet.el
@@ -2,9 +2,9 @@
 (yas-global-mode 1)
 
 ;; Personal snippets
-(setq yas/root-directory (concat user-emacs-directory "snippets"))
+(setq yas-root-directory (concat user-emacs-directory "snippets"))
 
 ;; Load the snippets
-(yas/load-directory yas/root-directory)
+(yas-load-directory yas-root-directory)
 
 (provide 'setup-yasnippet)
