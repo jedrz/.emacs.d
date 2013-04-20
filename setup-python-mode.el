e659094313e6c430ebf4b1696a385f8962c8afd6
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2013-04-20 19:07:29 +0200

    Run inferior python before eldoc

--- setup-python-mode.el
+++ setup-python-mode.el
@@ -2,6 +2,10 @@
 (after 'auto-complete
   (require 'ac-python))
 
+;; Run inferior python as eldoc requires it
+(add-hook 'python-mode-hook
+          (lambda ()
+            (run-python (python-shell-parse-command) nil nil)))
 (add-hook 'python-mode-hook 'turn-on-eldoc-mode)
 
 (provide 'setup-python-mode)
