388b27097fb46a1fc4d727b2d508f844e51cc362
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-02-25 20:47:57 +0100

    Complete local functions in octave buffers

--- setup-octave-mode.el
+++ setup-octave-mode.el
@@ -5,9 +5,18 @@
   ;; is printed in octave buffer.
   (setq inferior-octave-prompt "^[^>]+>+ "))
 
+(defun inferior-octave-update-current-directory ()
+  "Update current directory in octave buffer to the location of current file.
+
+This allows to local functions from current directory be completed."
+  (when inferior-octave-process
+    (inferior-octave-send-list-and-digest
+     (list (concat "cd " default-directory ";\n")))))
+
 (add-hook 'octave-mode-hook
           (lambda ()
             (auto-complete-mode 1)
-            (add-to-list 'ac-sources 'ac-source-octave)))
+            (add-to-list 'ac-sources 'ac-source-octave)
+            (inferior-octave-update-current-directory)))
 
 (provide 'setup-octave-mode)
