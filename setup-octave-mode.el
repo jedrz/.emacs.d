3a9e9f46e0a46d74cb4e5b4bde555c1a8ec83723
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-02-26 10:22:49 +0100

    Properly update current directory of octave process
    The directory needs to be updated whenever completion starts since
    edited buffers can be located in different directories.

--- setup-octave-mode.el
+++ setup-octave-mode.el
@@ -5,18 +5,17 @@
   ;; is printed in octave buffer.
   (setq inferior-octave-prompt "^[^>]+>+ "))
 
-(defun inferior-octave-update-current-directory ()
+(defadvice ac-octave-init
+  (after ac-octave-update-current-directoru activate compile)
   "Update current directory in octave buffer to the location of current file.
 
-This allows to local functions from current directory be completed."
-  (when inferior-octave-process
-    (inferior-octave-send-list-and-digest
-     (list (concat "cd " default-directory ";\n")))))
+This allows local functions from current directory to be completed."
+  (inferior-octave-send-list-and-digest
+   (list (concat "cd " default-directory ";\n"))))
 
 (add-hook 'octave-mode-hook
           (lambda ()
             (auto-complete-mode 1)
-            (add-to-list 'ac-sources 'ac-source-octave)
-            (inferior-octave-update-current-directory)))
+            (add-to-list 'ac-sources 'ac-source-octave)))
 
 (provide 'setup-octave-mode)
