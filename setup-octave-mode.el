a4fcaa566384771411fe195485fae59802bc3864
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-02-27 09:14:52 +0100

    Remove ac-octave's advice because was merged

--- setup-octave-mode.el
+++ setup-octave-mode.el
@@ -71,14 +71,6 @@
           (`oneline oneline)
           (`multiline multiline))))))
 
-(defadvice ac-octave-init
-  (after ac-octave-update-current-directoru activate compile)
-  "Update current directory in octave buffer to the location of current file.
-
-This allows local functions from current directory to be completed."
-  (inferior-octave-send-list-and-digest
-   (list (concat "cd " default-directory ";\n"))))
-
 (add-hook 'octave-mode-hook
           (lambda ()
             (run-octave t)
