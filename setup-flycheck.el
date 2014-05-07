98f17a6e6540599358a17bd66be6b8e494bfc007
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-05-07 15:11:07 +0200

    Display errors on one line in minibuffer

--- setup-flycheck.el
+++ setup-flycheck.el
@@ -4,6 +4,7 @@
 (after 'flycheck
   ;; Highlight whole line with error
   (setq flycheck-highlighting-mode 'lines)
+
   ;; Define a poor c/c++ checker (it fails when errors affect other files,
   ;; not the one being being checked actually)
   (defmacro flycheck-define-clike-checker (name command modes)
@@ -24,6 +25,18 @@
                                  ("g++" "-fsyntax-only" "-Wall" "-Wextra" "-std=c++11")
                                  c++-mode)
   (add-to-list 'flycheck-checkers 'c++-g++)
+
+  ;; Display error messages on one line in minibuffer and by new lines
+  ;; separated in `flycheck-error-message-buffer'.
+  (defun flycheck-diplay-error-messages-one-line (errors)
+    (-when-let (messages (-keep #'flycheck-error-message errors))
+      (when (flycheck-may-use-echo-area-p)
+        (message (s-join " | " messages))
+        (with-current-buffer (get-buffer-create flycheck-error-message-buffer)
+          (erase-buffer)
+          (insert (s-join "\n\n" messages))))))
+  (setq flycheck-display-errors-function
+        'flycheck-diplay-error-messages-one-line)
 
   (add-hook 'flycheck-mode-hook 'flycheck-cask-setup))
 
