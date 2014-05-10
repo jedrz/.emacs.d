b7de5dace2d7255f9f7e718aeefe3f7b412f587b
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-05-10 16:53:36 +0200

    Ignore .git directory in recent files

--- my-misc.el
+++ my-misc.el
@@ -14,7 +14,7 @@
   (setq recentf-save-file (concat user-emacs-directory "recentf")
         recentf-max-saved-items 200
         recentf-max-menu-items 15
-        recentf-exclude '("ido\\.last" "\\.mc-lists\\.el" "/elpa/")
+        recentf-exclude '("ido\\.last" "\\.mc-lists\\.el" "/elpa/" "\\.git/")
         recentf-auto-cleanup 'never))
 (recentf-mode 1)
 
