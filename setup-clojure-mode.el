9f86f817424ebe3dc4bef52463ffeba93c65e102
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-09-24 23:24:01 +0200

    Enable smarparens-strict-mode in clojure buffers

--- setup-clojure-mode.el
+++ setup-clojure-mode.el
@@ -1,3 +1,5 @@
+(add-hook 'clojure-mode-hook 'smartparens-strict-mode)
+
 ;; Cider
 (add-hook 'clojure-mode-hook 'cider-mode)
 (after 'cider
@@ -5,7 +7,9 @@
         cider-repl-pop-to-buffer-on-connect nil
         cider-repl-use-clojure-font-lock t)
 
-  (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode))
+  (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+
+  (add-hook 'cider-repl-mode-hook 'smartparens-strict-mode))
 
 ;; clj refactor
 (add-hook 'clojure-mode-hook
