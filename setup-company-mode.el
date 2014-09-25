db581d118405154cd75cc1a360a6d7b119cec558
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-09-25 09:22:21 +0200

    Add fci workaround for company

--- setup-company-mode.el
+++ setup-company-mode.el
@@ -16,4 +16,19 @@
 
   (setq yas-fallback-behavior '(apply company-indent-or-complete)))
 
+;; fci workaround - https://github.com/company-mode/company-mode/issues/180
+(defvar-local company-fci-mode-on-p nil)
+
+(defun company-turn-off-fci (&rest ignore)
+  (when (boundp 'fci-mode)
+    (setq company-fci-mode-on-p fci-mode)
+    (when fci-mode (fci-mode -1))))
+
+(defun company-maybe-turn-on-fci (&rest ignore)
+  (when company-fci-mode-on-p (fci-mode 1)))
+
+(add-hook 'company-completion-started-hook 'company-turn-off-fci)
+(add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
+(add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci)
+
 (provide 'setup-company-mode)
