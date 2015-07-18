186f6da057436ec703d7d2be5d0307cc33972668
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2015-07-18 19:21:38 +0200

    Add dots at the end of comments in init.el

--- init.el
+++ init.el
@@ -1,26 +1,26 @@
 ;; Speed up Emacs?
 (setq gc-cons-threshold 20000000)
 
-;; Don't load default library
+;; Don't load default library.
 (setq inhibit-default-init t)
 
-;; Don't load outdated byte-code files
+;; Don't load outdated byte-code files.
 (setq load-prefer-newer t)
 
 (defalias 'after 'with-eval-after-load)
 
-;; Set up load path
+;; Set up load path.
 (add-to-list 'load-path user-emacs-directory t)
 
-;; Add extensions' folder and sub-folders to load path
+;; Add extensions folder and sub-folders to load path.
 (let ((default-directory (concat user-emacs-directory "vendor")))
   (add-to-list 'load-path default-directory t)
   (normal-top-level-add-subdirs-to-load-path))
 
-;; Generate some autoloads and load them
+;; Generate some autoloads and load them.
 (require 'setup-autoloads)
 
-;; Configure package.el and install missing packages
+;; Configure package.el and install missing packages.
 (require 'setup-package)
 
 ;; TODO: Wrap with eval-when-compile later.
@@ -31,23 +31,23 @@
 (require 'diminish)
 (require 'dash)
 
-;; Separate custom file
+;; Separate custom file.
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file)
 
-;; Load appearance settings at the beginning to avoid momentary display
+;; Load appearance settings at the beginning to avoid momentary display.
 (require 'appearance)
 
-;; Shrink minor mode names
+;; Shrink minor mode names.
 (require 'setup-diminish)
 
-;; My sane defaults for emacs
+;; My sane defaults for emacs.
 (require 'sane-defaults)
 
-;; My misc
+;; My misc.
 (require 'my-misc)
 
-;; Load settings for some major modes
+;; Load settings for some major modes.
 (require 'setup-prog-mode)
 (require 'setup-org-mode)
 ;; Need to load clojure first, because some clojure hooks are added in
@@ -68,7 +68,7 @@
 (require 'setup-rust-mode)
 (require 'setup-major-modes)
 
-;; Setup extensions
+;; Setup extensions.
 (require 'setup-ido)
 (require 'setup-dired)
 (require 'setup-magit)
@@ -77,5 +77,5 @@
 (require 'setup-smartparens)
 (require 'setup-flycheck)
 
-;; Setup key bindings
+;; Setup key bindings.
 (require 'key-bindings)
