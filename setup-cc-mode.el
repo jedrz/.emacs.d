d542b4ce3db13b4647a6e490f25cb3f4183064ea
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2015-10-19 20:14:31 +0200

    Making irony mode to complete code

--- setup-cc-mode.el
+++ setup-cc-mode.el
@@ -31,7 +31,16 @@
   :init
   (progn
     (add-hook 'c++-mode-hook #'irony-mode)
-    (add-hook 'c-mode-hook #'irony-mode))
+    (add-hook 'c-mode-hook #'irony-mode)
+
+    (defun my-irony-mode-hook()
+      (define-key irony-mode-map [remap completion-at-point]
+        'irony-completion-at-point-async)
+      (define-key irony-mode-map [remap complete-symbol]
+        'irony-completion-at-point-async))
+    (add-hook 'irony-mode-hook #'my-irony-mode-hook)
+
+    (add-hook 'irony-mode-hook #'irony-cdb-autosetup-compile-options))
   :config
   (setq irony-additional-clang-options '("-std=c++11")))
 
