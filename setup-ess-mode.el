9c35fb50e23cb8ca84ac992cc3d108a1a87b16f2
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2016-04-17 18:30:58 +0200

    Run prog-mode hook for ess

--- setup-ess-mode.el
+++ setup-ess-mode.el
@@ -1,6 +1,8 @@
 (use-package ess
   :ensure t
   :mode ("\\.[rR]\\'" . R-mode)
+  :init
+  (add-hook 'ess-mode-hook (lambda () (run-hooks #'prog-mode-hook)))
   :config
   (setq ess-ask-for-ess-directory nil
         ess-eval-visibly nil))
