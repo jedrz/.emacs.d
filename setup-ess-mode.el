22d1befab6fa033156149d7145b46e619312d2bd
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2016-04-23 17:29:42 +0200

    Keep global .Rhistory file

--- setup-ess-mode.el
+++ setup-ess-mode.el
@@ -5,6 +5,8 @@
   (add-hook 'ess-mode-hook (lambda () (run-hooks #'prog-mode-hook)))
   :config
   (setq ess-ask-for-ess-directory nil
-        ess-eval-visibly nil))
+        ess-eval-visibly nil
+        ;; Keep global .Rhistory file.
+        ess-history-directory "~/.R/"))
 
 (provide 'setup-ess-mode)
