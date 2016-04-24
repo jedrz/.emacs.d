3d310411bbdd25c6af1d071f296c8ef9bb44510f
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2016-04-24 14:19:03 +0200

    Flush R history to file

--- setup-ess-mode.el
+++ setup-ess-mode.el
@@ -4,9 +4,21 @@
   :init
   (add-hook 'ess-mode-hook (lambda () (run-hooks #'prog-mode-hook)))
   :config
-  (setq ess-ask-for-ess-directory nil
-        ess-eval-visibly nil
-        ;; Keep global .Rhistory file.
-        ess-history-directory "~/.R/"))
+  (progn
+    (setq ess-ask-for-ess-directory nil
+          ess-eval-visibly nil
+          ;; Keep global .Rhistory file.
+          ess-history-directory "~/.R/")
+
+    ;; Workaround to flush R history to file.
+    ;; Apply for all buffers since emacs doesn't run `kill-buffer-hook'
+    ;; which has `ess-kill-buffer-function'.
+    ;; For persistent history in all buffers check:
+    ;; https://oleksandrmanzyuk.wordpress.com/2011/10/23/a-persistent-command-history-in-emacs/
+    (add-hook #'kill-emacs-hook (lambda ()
+                                  (mapc (lambda (buffer)
+                                          (with-current-buffer buffer
+                                            (ess-kill-buffer-function)))
+                                        (buffer-list))))))
 
 (provide 'setup-ess-mode)
