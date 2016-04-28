13b8ca768c020f5b00f5201e828e0c43121d0eff
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2016-04-29 00:09:42 +0200

    Split configuration of swiper after ivy 0.8 release

--- setup-swiper.el
+++ setup-swiper.el
@@ -4,62 +4,100 @@
   :defer t)
 
 ;; isearch with an overview.
-(use-package counsel
+(use-package ivy
   :ensure t
   :defer t
   :bind
-  (("C-s" . swiper)
-   ("C-r" . swiper)
-   ("C-S" . swiper-use-region)
-   ("C-R" . swiper-use-region)
-   ("C-c C-r" . ivy-resume)
-   ("M-x" . counsel-M-x)
-   ("C-x C-f" . counsel-find-file)
-   ("C-x 4 C-f" . counsel-find-file-other-window)
-   ("C-x C-i" . counsel-imenu)
+  (("C-c C-r" . ivy-resume)
    ("C-x f" . ivy-recentf)
-   ("C-x 4 f" . ivy-recentf-other-window)
-   ("C-h f" . counsel-describe-function)
-   ("C-h v" . counsel-describe-variable)
-   ("C-h S" . counsel-info-lookup-symbol))
+   ("C-x 4 f" . ivy-recentf-other-window))
   :commands
-  (swiper-use-region
-   counsel-find-file-other-window
-   ivy-recentf-other-window)
+  (ivy-recentf-other-window)
   :init
   (ivy-mode 1)
   :config
   (progn
     (setq ivy-use-virtual-buffers t
           ivy-re-builders-alist '((t . ivy--regex-fuzzy))
-          ivy-initial-inputs-alist nil
-          counsel-find-file-at-point t)
+          ivy-initial-inputs-alist nil)
 
     ;; Abbreviate /home/user with ~.
-    (ivy-set-display-transformer 'ivy-recentf 'abbreviate-file-name)
+    (ivy-set-display-transformer #'ivy-recentf #'abbreviate-file-name)
 
     (ivy-set-actions
-     'counsel-find-file
+     #'counsel-find-file
      '(("j" find-file-other-window "other")
        ("s" ivy-sudo-edit-action "sudo")))
 
     (ivy-set-actions
-     'counsel-find-file-other-window
+     #'counsel-find-file-other-window
      '(("s" ivy-sudo-edit-action)))
 
     (ivy-set-actions
-     'ivy-recentf
+     #'ivy-recentf
      '(("j" find-file-other-window "other")))
 
-    (defun swiper-use-region ()
-      "Search for active region or input."
+    (defun ivy-recentf-other-window ()
+      "Find a file on `recentf-list' in another window."
       (interactive)
-      (if (region-active-p)
-          (let ((selection (buffer-substring-no-properties
-                            (region-beginning) (region-end))))
-            (deactivate-mark)
-            (swiper selection))
-        (call-interactively 'swiper)))
+      (ivy-read "Recentf: " recentf-list
+                :action #'find-file-other-window
+                :caller 'ivy-recentf-other-window))
+
+    (defun ivy-sudo-edit-action (file-name)
+      (with-ivy-window
+        (find-file (expand-file-name
+                    (concat "/sudo:root@localhost:" file-name)
+                    ivy--directory))))))
+
+(use-package swiper
+  :ensure t
+  :defer t
+  :bind
+  (("C-s" . swiper)
+   ("C-r" . swiper)
+   ("C-S" . swiper-use-region)
+   ("C-R" . swiper-use-region))
+  :commands
+  (swiper-use-region)
+  :init
+  (ivy-mode 1)
+  :config
+  (defun swiper-use-region ()
+    "Search for active region or input."
+    (interactive)
+    (if (region-active-p)
+        (let ((selection (buffer-substring-no-properties
+                          (region-beginning) (region-end))))
+          (deactivate-mark)
+          (swiper selection))
+      (call-interactively #'swiper))))
+
+(use-package counsel
+  :ensure t
+  :defer t
+  :bind
+  (("M-x" . counsel-M-x)
+   ("C-x C-f" . counsel-find-file)
+   ("C-x 4 C-f" . counsel-find-file-other-window)
+   ("C-x C-i" . counsel-imenu)
+   ("C-h f" . counsel-describe-function)
+   ("C-h v" . counsel-describe-variable)
+   ("C-h S" . counsel-info-lookup-symbol))
+  :commands
+  (counsel-find-file-other-window)
+  :config
+  (progn
+    (setq counsel-find-file-at-point t)
+
+    (ivy-set-actions
+     #'counsel-find-file
+     '(("j" find-file-other-window "other")
+       ("s" ivy-sudo-edit-action "sudo")))
+
+    (ivy-set-actions
+     #'counsel-find-file-other-window
+     '(("s" ivy-sudo-edit-action)))
 
     (defun counsel-find-file-other-window (&optional initial-input)
       "Forward to `find-file-other-window'.
@@ -74,21 +112,8 @@ When INITIAL-INPUT is non-nil, use it in the minibuffer during completion."
                 :preselect (when counsel-find-file-at-point
                              (require 'ffap)
                              (ffap-guesser))
-                :require-match 'confirm-after-completion
-                :history 'file-name-history
-                :keymap counsel-find-file-map))
-
-    (defun ivy-recentf-other-window ()
-      "Find a file on `recentf-list' in another window."
-      (interactive)
-      (ivy-read "Recentf: " recentf-list
-                :action #'find-file-other-window
-                :caller 'ivy-recentf-other-window))
-
-    (defun ivy-sudo-edit-action (file-name)
-      (with-ivy-window
-        (find-file (expand-file-name
-                    (concat "/sudo:root@localhost:" file-name)
-                    ivy--directory))))))
+                :require-match #'confirm-after-completion
+                :history #'file-name-history
+                :keymap counsel-find-file-map))))
 
 (provide 'setup-swiper)
