d453065e8e9a92ab50d05e1469731eadaf281faa
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2016-05-02 14:53:00 +0200

    Add default goto-line to avy hydra

--- general-settings.el
+++ general-settings.el
@@ -418,6 +418,7 @@
      ("C" avy-goto-char-2 "2 char")
      ("m" avy-goto-char-in-line "char in line")
      ("g" avy-goto-line "line")
+     ("G" goto-line "line (default)")
      ("w" avy-goto-word-1 "word")
      ("W" avy-goto-word-0 "some word")
      ("s" avy-goto-subword-1 "subword")
