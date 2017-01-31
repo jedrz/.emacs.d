22efa2ee4be15edd127feaa301ff221651525f91
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2017-01-31 14:16:51 +0100

    Don't run inferior python for python files

--- lisp/setup-python-mode.el
+++ lisp/setup-python-mode.el
@@ -3,9 +3,10 @@
   :config
   (progn
     ;; Run inferior python as eldoc requires it
-    (add-hook 'python-mode-hook
-              (lambda ()
-                (run-python (python-shell-parse-command) nil nil)))
+    ;; Well, maybe not.
+    ;; (add-hook 'python-mode-hook
+    ;;           (lambda ()
+    ;;             (run-python (python-shell-parse-command) nil nil)))
     (add-hook 'python-mode-hook #'turn-on-eldoc-mode)
 
     (add-hook 'inferior-python-mode-hook #'turn-on-comint-history)))
