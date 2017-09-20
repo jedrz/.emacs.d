e957eabad49e2f01a608774dec1eade603f177dc
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2017-09-20 18:38:38 +0200

    Fix backup directory

--- lisp/general-settings.el
+++ lisp/general-settings.el
@@ -25,7 +25,7 @@
 
 ;; Backup files settings.
 (setq backup-by-copying t
-      backup-directory-alist (list (cons "." "~/local/share/emacs/backups"))
+      backup-directory-alist (list (cons "." "~/.local/share/emacs/backups"))
       delete-old-versions t
       kept-new-versions 6
       kept-old-versions 2
