53dfb623a9477808547a6ae64f30d3b3f7a7161a
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2018-10-28 20:48:37 +0100

    Clean up custom.el

--- lisp/custom.el
+++ lisp/custom.el
@@ -3,52 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   (vector "#4d4d4c" "#c82829" "#718c00" "#eab700" "#4271ae" "#8959a8" "#3e999f" "#ffffff"))
  '(custom-safe-themes
    (quote
     ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
- '(fci-rule-color "#d6d6d6")
  '(package-selected-packages
    (quote
     (org-gcal orgtbl-aggregate org-aggregate org-plus-contrib magit magit-popup exec-path-from-shell company-terraform company-terraform-mode terraform-mode pdf-tools anaconda-mode spaceline webpaste auth-password-store tiny page-break-lines dired-open string-inflection org-download indium json-navigator beginend pomidor virtualenvwrapper ivy-hydra ox-reveal elm-mode kotlin-mode ag zencoding-mode yaml-mode wttrin which-key wgrep visual-regexp use-package undo-tree typescript-mode toml-mode tagedit solarized-theme smex smartparens smart-mode-line slime-company robe refheap rainbow-mode rainbow-delimiters racer pyenv-mode projectile pkgbuild-mode paradox org-journal org-bullets move-text material-theme markdown-mode magit-gh-pulls latex-extra js2-refactor highlight-symbol highlight-indentation google-this git-timemachine git-messenger gist free-keys flycheck-rust flycheck-cask flx-ido flatui-theme fill-column-indicator fancy-narrow evil-numbers ess elisp-slime-nav dockerfile-mode discover-my-major dired-imenu dired+ diff-hl counsel company-tern company-shell company-irony company-auctex company-anaconda color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized clj-refactor change-inner buffer-move browse-kill-ring auctex-latexmk anzu)))
  '(paradox-github-token t)
  '(safe-local-variable-values
    (quote
-    ((TeX-command-extra-options . "-shell-escape")
-     (eval venv-workon "pdm")
-     (python-shell-extra-pythonpaths "/home/lukasz/Programowanie/pdm/predict")
-     (python-shell-extra-pythonpaths "/home/lukasz/Programowanie/python/predicting-jams"))))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#c82829")
-     (40 . "#f5871f")
-     (60 . "#eab700")
-     (80 . "#718c00")
-     (100 . "#3e999f")
-     (120 . "#4271ae")
-     (140 . "#8959a8")
-     (160 . "#c82829")
-     (180 . "#f5871f")
-     (200 . "#eab700")
-     (220 . "#718c00")
-     (240 . "#3e999f")
-     (260 . "#4271ae")
-     (280 . "#8959a8")
-     (300 . "#c82829")
-     (320 . "#f5871f")
-     (340 . "#eab700")
-     (360 . "#718c00"))))
- '(vc-annotate-very-old-color nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:inherit outline-1 :height 1.4))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.1)))))
+    ((TeX-command-extra-options . "-shell-escape")))))
