69849cef86c3d5d2a4559cd01809cdeb76a6eb57
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2019-02-22 21:04:15 +0100

    Disable ess kill emacs hook

--- lisp/setup-ess-mode.el
+++ lisp/setup-ess-mode.el
@@ -9,18 +9,6 @@
           ess-eval-visibly nil
           ;; Keep global .Rhistory file.
           ess-history-directory "~/.R/")
-
-    ;; Workaround to flush R history to file.
-    ;; Apply for all buffers since emacs doesn't run `kill-buffer-hook'
-    ;; which has `ess-kill-buffer-function'.
-    ;; For persistent history in all buffers check:
-    ;; https://oleksandrmanzyuk.wordpress.com/2011/10/23/a-persistent-command-history-in-emacs/
-    (add-hook #'kill-emacs-hook (lambda ()
-                                  (mapc (lambda (buffer)
-                                          (with-current-buffer buffer
-                                            (ess-kill-buffer-function)))
-                                        (buffer-list))))
-
     (with-eval-after-load 'flycheck
       (setq flycheck-lintr-linters "with_defaults(camel_case_linter = NULL, snake_case_linter, object_usage_linter = NULL)"))))
 
