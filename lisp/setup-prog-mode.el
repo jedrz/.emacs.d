59b838794bf22b9c64a121a83e445d9f5fe3429d
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2019-09-20 21:32:11 +0200

    Add dumb-jump

--- lisp/setup-prog-mode.el
+++ lisp/setup-prog-mode.el
@@ -42,8 +42,19 @@
   :init
   (add-hook 'prog-mode-hook #'hl-todo-mode))
 
-(use-package ag
+(use-package dumb-jump
   :ensure t
-  :defer t)
+  :defer t
+  :init
+  (bind-key "C-M-g"
+            (defhydra dumb-jump-hydra (:color blue :columns 3)
+              "Dumb Jump"
+              ("j" dumb-jump-go "Go")
+              ("o" dumb-jump-go-other-window "Other window")
+              ("e" dumb-jump-go-prefer-external "Go external")
+              ("x" dumb-jump-go-prefer-external-other-window "Go external other window")
+              ("i" dumb-jump-go-prompt "Prompt")
+              ("l" dumb-jump-quick-look "Quick look")
+              ("b" dumb-jump-back "Back"))))
 
 (provide 'setup-prog-mode)
