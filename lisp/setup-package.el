f4f618adf887a4de23b1b8d399301ce4fa31d973
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2020-09-15 19:52:16 +0200

    Update melpa url

--- lisp/setup-package.el
+++ lisp/setup-package.el
@@ -6,7 +6,7 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
 
