831d23e6596d5be34d23d45e59ace5c8f0ed208f
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2023-04-01 19:25:08 +0200

    [fci] Remove unused hooks

--- lisp/setup-company-mode.el
+++ lisp/setup-company-mode.el
@@ -27,21 +27,6 @@
             (let ((yas-fallback-behavior #'call-other-command))
               (yas--fallback)))))
 
-      (setq yas-fallback-behavior '(apply company-indent-or-complete)))
-
-    ;; fci workaround - https://github.com/company-mode/company-mode/issues/180
-    (defvar-local company-fci-mode-on-p nil)
-
-    (defun company-turn-off-fci (&rest ignore)
-      (when (boundp 'fci-mode)
-        (setq company-fci-mode-on-p fci-mode)
-        (when fci-mode (fci-mode -1))))
-
-    (defun company-maybe-turn-on-fci (&rest ignore)
-      (when company-fci-mode-on-p (fci-mode 1)))
-
-    (add-hook 'company-completion-started-hook #'company-turn-off-fci)
-    (add-hook 'company-completion-finished-hook #'company-maybe-turn-on-fci)
-    (add-hook 'company-completion-cancelled-hook #'company-maybe-turn-on-fci)))
+      (setq yas-fallback-behavior '(apply company-indent-or-complete)))))
 
 (provide 'setup-company-mode)
