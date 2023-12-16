bc0c840dc576a27ac31604e11101df1c8fa65ac2
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2023-12-17 00:22:33 +0100

    Refresh haskell mode config

--- lisp/setup-haskell-mode.el
+++ lisp/setup-haskell-mode.el
@@ -1,24 +1,14 @@
-;; https://github.com/serras/emacs-haskell-tutorial
-
 (use-package haskell-mode
   :ensure t
   :defer t
-  :config
-  (progn
-    (bind-key "C-c C-o" #'haskell-compile haskell-mode-map)
-    (bind-key "C-c C-l" #'haskell-process-load-or-reload haskell-mode-map)
-    (bind-key "C-c C-z" #'haskell-interactive-switch haskell-mode-map)
-    (bind-key "C-c C-n C-t" #'haskell-process-do-type haskell-mode-map)
-    (bind-key "C-c C-n C-i" #'haskell-process-do-info haskell-mode-map)
-    (bind-key "C-c C-n C-c" #'haskell-process-cabal-build haskell-mode-map)
-    (bind-key "C-c C-n c" #'haskell-process-cabal haskell-mode-map)))
+  :hook
+  (haskell-mode . interactive-haskell-mode))
 
-(use-package hindent
+(use-package lsp-haskell
   :ensure t
   :defer t
-  :init
-  (progn
-    (setq hindent-process-path (expand-file-name "~/.local/bin/hindent"))
-    (add-hook 'haskell-mode-hook #'hindent-mode)))
+  :hook
+  ((haskell-mode . lsp)
+   (haskell-literate-mode . lsp)))
 
 (provide 'setup-haskell-mode)
