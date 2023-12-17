06b3b4b7e5f58c5fc631e8a9e34784e0f7285e4c
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2023-12-17 17:52:48 +0100

    Add haskell code formatter

--- lisp/setup-haskell-mode.el
+++ lisp/setup-haskell-mode.el
@@ -11,4 +11,12 @@
   ((haskell-mode . lsp)
    (haskell-literate-mode . lsp)))
 
+(use-package ormolu
+  :ensure t
+  :defer t
+  :hook (haskell-mode . ormolu-format-on-save-mode)
+  :bind
+  (:map haskell-mode-map
+        ("C-c r" . ormolu-format-buffer)))
+
 (provide 'setup-haskell-mode)
